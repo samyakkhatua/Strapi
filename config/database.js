fe6dd4a24e297d934d332a871f51f6ae3b656a61
samyakkhatua <samyakkhatua@duck.com>
2022-11-02 19:01:03 +0530

    demo content type added

--- config/database.js
+++ config/database.js
@@ -1,13 +1,25 @@
-module.exports = ({ env }) => ({
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('PGHOST', '127.0.0.1'),
+//       port: env.int('PGPORT', 5432),
+//       database: env('PGDATABASE', 'strapi'),
+//       user: env('PGUSER', 'strapi'),
+//       password: env('PGPASSWORD', 'password'),
+//       ssl: env.bool(true),
+//     },
+//   },
+// });
+
+const path = require("path");
+
+module.exports = () => ({
   connection: {
-    client: 'postgres',
+    client: "sqlite",
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
+      filename: path.join(__dirname, "..", ".tmp/data.db"),
     },
+    useNullAsDefault: true,
   },
 });
